ba20020eb673d3738a2ebcf092374e0b3e36df1f
Jude-Hageman <131105266+Jude-Hageman@users.noreply.github.com>
2023-06-12 21:07:54 -0400

    Add files via upload

---
+++ starter/catpoint-parent/src/cd0384-java-application-deployment-projectstarter/starter/catpoint-parent/securityService/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module securityService {
+    requires imageService;
+    requires com.miglayout.swing;
+    requires java.desktop;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+
+}
